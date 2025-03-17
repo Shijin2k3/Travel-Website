d8531e154021d5ba8638f8dc4ddaf474dbbb49a5
Shijin2k3 <shijinkumar2003@gmail.com>
2025-03-17 09:51:53 +0530

    Initialized Mobile Navbar

--- components/Home/Navbar/Navbar.tsx
+++ components/Home/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import { TbAirBalloon } from 'react-icons/tb'
 
 const Navbar = () => {
   return (
-    <div className='bg-blue-950 transition-all duration-200 h-[12vh] z-[1000]'>
+    <div className='bg-blue-950 transition-all duration-200 h-[12vh] z-[1000] fixed w-full'>
         <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
             <div className='flex items-center space-x-2'>
                 <div className='w-10 h-10 bg-rose-500 rounded-full flex items-center 
