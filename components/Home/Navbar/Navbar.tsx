e12f59b45937904497697e94b3571f46109c9e0b
Shijin2k3 <shijinkumar2003@gmail.com>
2025-03-17 13:21:33 +0530

    Initialized search box in Hero Page

--- components/Home/Navbar/Navbar.tsx
+++ components/Home/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
+'use client'
 import { navLinks } from '@/constant/constant'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { HiMiniBars3BottomLeft } from 'react-icons/hi2'
 import { TbAirBalloon } from 'react-icons/tb'
 
@@ -9,15 +10,28 @@ type Props={
 }
 
 const Navbar = ({openNav}:Props) => {
+
+  const [navBg,setNavBg]=useState(false);
+
+  useEffect(()=>{
+   const handler=()=>{
+    if(window.scrollY >= 90){ setNavBg(true)}
+    if(window.scrollY <= 90) {setNavBg(false)}
+   }
+  window.addEventListener('scroll',handler);
+  return ()=>window.removeEventListener('scroll',handler);
+  },[])
+
+    
   return (
-    <div className='bg-blue-950 transition-all duration-200 h-[12vh] z-[1000] fixed w-full'>
+    <div className={`bg-blue-950  ${navBg?'bg-blue-950 shadow-md':'fixed'} transition-all duration-200 h-[12vh] z-[1000] fixed w-full`}>
         <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
             <div className='flex items-center space-x-2'>
                 <div className='w-10 h-10 bg-rose-500 rounded-full flex items-center 
                 justify-center flex-col'>
                     <TbAirBalloon  className='w-6 h-6 text-white'/>
                 </div>
-                <h1 className='text-xl md:text-2xl text-white uppercase font-bold'>Travel</h1>
+                <h1 className='text-xl md:text-2xl text-white uppercase font-bold'>Trip</h1>
             </div>
             <div className='hidden lg:flex items-center space-x-10'>
                 {navLinks.map((link)=>{
