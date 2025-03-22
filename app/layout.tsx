6983c42c1f2d6b0e9bef1ff32547ab07d877a252
Shijin2k3 <shijinkumar2003@gmail.com>
2025-03-22 11:30:22 +0530

    Initialized ScrollToTop

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import {Poppins} from "next/font/google"
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "@/components/Home/Footer/Footer";
+import ScrollToTop from "@/components/Helper/ScrollToTop";
 
 
 const font=Poppins({
@@ -39,6 +40,7 @@ export default function RootLayout({
         <ResponsiveNav />
         {children}
         <Footer />
+        <ScrollToTop />
         
       </body>
     </html>
